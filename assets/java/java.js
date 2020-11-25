4ddc92637d82996ced6245a5eef5e30217d94e83
djrowley2104 <djrowley@hotmail.co.uk>
2020-11-25 22:14:29 +0000

    Docs; format changes to make the email function better

--- assets/java/java.js
+++ assets/java/java.js
@@ -9,11 +9,7 @@
 			document.getElementById('shopdetail').classList.remove('hide');
 	};
 		
-	  
-	  
-	  
-	  
-    function initMap(){
+	function initMap(){
       /* Map options, sets Zoom and location of centre */
       var options = {
         zoom:10,
@@ -35,16 +31,16 @@
         {
           coords:{lat:51.7661148,lng:-2.556003},
           title: "Dean Forest Cycles",
-          phone: "01594 368009",
-		  content:"<div><h2>Dean Forest Cycles</h2><p>Cafe and accessory Sale</p></div>" +
-		  "<div><p>Tele: 01594 368009</p></div>" + "<img src='./assets/figures/dean.png'></img>" + "<a onClick='load_shop_detail' class='detail' href='#'> book a service</a>"
+          phone: "<img src='./assets/figures/phone.png'></img>" + " 01594 368009",
+		  content:"<div><h2>Dean Forest Cycles</h2><p>Cafe, Spares and Accessory Sales</p></div>" +
+		  "<div><p>Tele: 01594 368009</p></div>" + "<img src='./assets/figures/dean.png'></img>"
         },
         {
 		  coords:{lat:51.809485,lng:-2.5692920},
           title: "Peddle a Bike Away",
-		  phone: "01594 729000",
-          content:"<h2>Peddle a Bike Away</h2><p>Cycle center, cafe, and bike repairs</p>" +
-          "<p>Tele: 01594 729000</p>" + "<img src='./assets/figures/peddle.png'></img>" + "<a onClick='load_shop_detail' class='detail' href='#'> book a service</a>"
+		  phone: "<img src='./assets/figures/phone.png'></img>" + " 01594 729000",
+          content:"<h2>Peddle a Bike Away</h2><p>Cycle Center, Cafe, and Bike Repairs</p>" +
+          "<p>Tele: 01594 729000</p>" + "<img src='./assets/figures/peddle.png'></img>" 
 		  
 		  
         },
@@ -52,9 +48,9 @@
           
           coords:{lat:51.8333019,lng:-2.5119103},
           title: "Sprung Suspension",
-          phone: "01594 729030",
+          phone: "<img src='./assets/figures/phone.png'></img>" + " 01594 729030",
 		  content:"<h2>Sprung Suspension</h2><p>For all your suspension repairs and wheel trueing</p>" +
-          "<p>Tele: 01594 729030</p>" + "<img src='./assets/figures/sprung.png'></img>" + "<a onClick='load_shop_detail' class='detail' href='#'> book a service</a>"
+          "<p>Tele: 01594 729030</p>" + "<img src='./assets/figures/sprung.png'></img>" 
 		
 		},
 
@@ -62,9 +58,9 @@
          
           coords:{lat:51.790908,lng:-3.0034466},
           title: "Treads N Trail",
-          phone: "01633 729030",
+          phone: "<img src='./assets/figures/phone.png'></img>" + " 01633 729030",
           content:"<h2>Treads N Trails</h2><p>For all your suspension repairs and wheel trueing</p>" +
-          "<p>Tele: 01633 729030</p>" + "<img src='./assets/figures/treads.png'></img>" + "<a onClick='load_shop_detail' class='detail' href='#'> book a service</a>"
+          "<p>Tele: 01633 729030</p>" + "<img src='./assets/figures/treads.png'></img>" 
           
           
 	  
@@ -73,25 +69,20 @@
           
           coords:{lat:51.87542254,lng:-2.9832272},
           title: "Black Mountain Cycle Centre",
-          phone: "01633 729111",
+          phone: "<img src='./assets/figures/phone.png'></img>" + " 01633 729111",
 		  content:"<h2>Black Mountain Cycle Centre</h2><p>Trail Centre (Basic)</p>" +
-          "<p>Tele: 01633 729111</p>" + "<img src='./assets/figures/BMcycle.png'></img>" + "<a onClick='load_shop_detail' class='detail' href='#'> book a service</a>"
+          "<p>Tele: 01633 729111</p>" + "<img src='./assets/figures/BMcycle.png'></img>" 
 	  
 	     },
 {
           
           coords:{lat:51.7148259,lng:-3.3707652},
           title: "BikePark Wales",
-          phone: "01633 729791",
+          phone: "<img src='./assets/figures/phone.png'></img>" + " 01633 729791",
           content:"<h2>BikePark Wales</h2><p>Trail centre, cafe, and bike repairs</p>" +
-          "<p>Tele: 01633 729791</p>" + "<img src='./assets/figures/bike.png'></img>" + "<a onClick='load_shop_detail' class='detail' href='#'> book a service</a>"
+          "<p>Tele: 01633 729791</p>" + "<img src='./assets/figures/bike.png'></img>" 
 	  
-        }
-        
-        
-               
-        
-      ];
+        }];
 
       /* Loop through markers */
       for(var i = 0;i < markers.length;i++){
